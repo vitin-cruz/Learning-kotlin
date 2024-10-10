9c471f67312ec463f88a6247618f4e2715fe4585
Vitor Joaquim <vitcruz26@gmail.com>
2024-10-10 18:13:37 -0300

    Testing input method.

--- app/src/main/java/com/example/first_kotlin_code/MainActivity.kt
+++ app/src/main/java/com/example/first_kotlin_code/MainActivity.kt
@@ -1,5 +1,13 @@
 val firstMessage: kotlin.String = "Hello World, I'm Vitor"
 
 fun main(){
-    print(firstMessage)
+    println(firstMessage)
+
+    // Testing input and output
+    println("Enter any words: ")
+    val word = readln()
+    println("Your digited word is: " + word)
 }
+
+
+
